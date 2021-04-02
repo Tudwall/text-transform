98ad80a84d9ef05dc74741bf26153042ed5fc706
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-04-02 21:09:55 +0200

    added todo comments

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+// Selectors
+const input = document.querySelector("#input");
+
+// Event Listeners
+// Functions
+function transformText(inputValue) {
+  // Get input value
+  // Transfom into array
+  // Scan array to find character code
+  // Check if code is between 65 and 90
+  // if false, return non converted char
+  // if true, check if you need to go forward or backward 13
+}
+
 // Get textarea content, modify it and inject it in a result div.
