1b13058a49654eb41cbd4d632ac7c73279b272db
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-04-03 02:31:52 +0200

    wrote transformText function

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const input = document.querySelector("#input");
 
 // Event Listeners
 // Functions
-function transformText(inputValue) {
-  // Get input value
-  // Transfom into array
-  // Scan array to find character code
-  // Check if code is between 65 and 90
-  // if false, return non converted char
-  // if true, check if you need to go forward or backward 13
+function transformText(str) {
+  // Get a new var with the string, turn it into an array
+  const newStr = str.split('').map(char => {
+    const code = char.charCodeAt(char)
+    // Scans array to find the character's code
+    if (code < 65 || code > 90) {
+      // Return a converted character
+      return String.fromCharCode(code)
+    } else if {
+      // Forward 13
+      return String.fromCharCode(code + 13)
+    } else {
+      // Backward 13
+      return String.fromCharCode(code - 13)
+    }
+  }).join('') // To get it back as a string
+  return newStr
 }
 
 // Get textarea content, modify it and inject it in a result div.
