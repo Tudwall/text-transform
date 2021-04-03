19cafa88c6f5747e15633cc8699e19116e98f2c4
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-04-03 02:37:34 +0200

    added eventListener frame and comment instructions

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 // Selectors
 const input = document.querySelector("#input");
+const submit = document.querySelector("submit");
 
 // Event Listeners
+submit.addEventListener(
+  "click",
+  (trans = (e) => {
+    // Take input value and pass it as a transformText param.
+  })
+);
+
 // Functions
 function transformText(str) {
-  // Get a new var with the string, turn it into an array
+  // Get a new var with the string, turn it into an array.
   return str
     .split("")
     .map((char) => {
       const code = char.charCodeAt(char);
-      // Scans array to find the character's code
+      // Scans array to find the character's code.
       if (code < 65 || code > 90) {
-        // Return a converted character
+        // Return a converted character.
         return String.fromCharCode(code);
       } else if (code < 78) {
-        // Forward 13
+        // Forward 13.
         return String.fromCharCode(code + 13);
       } else {
-        // Backward 13
+        // Backward 13.
         return String.fromCharCode(code - 13);
       }
     })
-    .join(""); // To get it back as a string
+    .join(""); // To get it back as a string.
 }
 
 // Get textarea content, modify it and inject it in a result div.
