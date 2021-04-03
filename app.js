b97d3fbc13a633b43512afb846434e0cbec33b63
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-04-03 02:33:51 +0200

    refactoring and fixed bug

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const input = document.querySelector("#input");
 // Functions
 function transformText(str) {
   // Get a new var with the string, turn it into an array
-  const newStr = str.split('').map(char => {
-    const code = char.charCodeAt(char)
-    // Scans array to find the character's code
-    if (code < 65 || code > 90) {
-      // Return a converted character
-      return String.fromCharCode(code)
-    } else if {
-      // Forward 13
-      return String.fromCharCode(code + 13)
-    } else {
-      // Backward 13
-      return String.fromCharCode(code - 13)
-    }
-  }).join('') // To get it back as a string
-  return newStr
+  return str
+    .split("")
+    .map((char) => {
+      const code = char.charCodeAt(char);
+      // Scans array to find the character's code
+      if (code < 65 || code > 90) {
+        // Return a converted character
+        return String.fromCharCode(code);
+      } else if (code < 78) {
+        // Forward 13
+        return String.fromCharCode(code + 13);
+      } else {
+        // Backward 13
+        return String.fromCharCode(code - 13);
+      }
+    })
+    .join(""); // To get it back as a string
 }
 
 // Get textarea content, modify it and inject it in a result div.
