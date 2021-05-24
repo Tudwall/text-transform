b5b440995c80d432a82720b5bd81a18f3906b2dd
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-24 18:06:17 +0200

    select elements

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // Selectors
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const result = document.querySelector("#result");
+const offset = document.querySelector("#offset");
 
 // Functions
 
 // Event Listeners
+
+/* Algorithme
+   Selectionner input, submit, result et offset
+
+   Récupérer le texte dans input
+   Récupérer le int dans offset
+
+   Mettre input en array (split)
+   Décaler tout les caractères dans la str selon offset(map pour appliquer à chaque char)
+   Remettre input en str
+   Retourner la str transformée dans result quand submit est clické
+*/
+
+// A-Z: 65-90
+// a-z: 97-122
 
 // Test texts
 // URYYB JBEYQ
